90271c25319488c2e90a62deefeae6977377984d
Романчук Константин Дмитриевич <83108@npo.local>
2024-12-28 09:51:09 +0600

    Create base component for icons

---
+++ portfolio-folder/src/components/icons/SvgHolder.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <svg class="disabled" viewBox="0 0 20 20">
+    <defs>
+      <!--Иконка по умолчанию-->
+      <symbol id="BaseIcon">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 18C14.9183 18 18.5 14.4183 18.5 10C18.5 5.58172 14.9183 2 10.5 2C6.08172 2 2.5 5.58172 2.5 10C2.5 14.4183 6.08172 18 10.5 18ZM10.5 17C6.63401 17 3.5 13.866 3.5 10C3.5 6.13401 6.63401 3 10.5 3C14.366 3 17.5 6.13401 17.5 10C17.5 13.866 14.366 17 10.5 17Z" />
+      </symbol>
+      <symbol id="filledBaseIcon">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 18C14.9183 18 18.5 14.4183 18.5 10C18.5 5.58172 14.9183 2 10.5 2C6.08172 2 2.5 5.58172 2.5 10C2.5 14.4183 6.08172 18 10.5 18Z" />
+      </symbol>
+    </defs>
+  </svg>
+</template>
