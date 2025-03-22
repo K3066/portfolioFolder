cedecb7e916bbe7b41cddb1aed3d39783013f84a
Konstantin <k.rom3066@gmail.com>
2025-03-22 16:22:17 +0600

    Add 404 folder

--- portfolio-folder/src/router/index.ts
+++ portfolio-folder/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundFolder from "@/views/NotFoundFolder.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: "/not-found", name: "notFound", component: NotFoundFolder
+    },
+    {
+      path: "/*", redirect: '/not-found'
+    }
   ],
 })
 
