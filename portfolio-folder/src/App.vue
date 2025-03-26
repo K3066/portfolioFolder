39c420337660a49442c04a5bb62338de593cc013
Konstantin <k.rom3066@gmail.com>
2025-03-26 19:16:39 +0600

    Import SvgHolder

--- portfolio-folder/src/App.vue
+++ portfolio-folder/src/App.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import FolderHeader from "@/components/FolderHeader.vue";
 import BorderLayer from "@/components/BorderLayer.vue";
 </script>
 
 <template>
-  <folder-header />
-  <router-view class="folder"/>
-  <border-layer/>
-
+  <svg-holder style="display: none"/>
+  <router-view />
 </template>
 
-<style scoped lang="sass">
-.folder
-  height: 1px
-  background: green
-  display: flex
-  flex-grow: 1
-  position: relative
-  padding: 0.75rem
-
-</style>
--- portfolio-folder/src/main.ts
+++ portfolio-folder/src/main.ts
@@ -5,8 +5,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import SvgHolder from "@/components/icons/SvgHolder.vue";
 
 const app = createApp(App)
+app.component('SvgHolder', SvgHolder)
 
 app.use(createPinia())
 app.use(router)
