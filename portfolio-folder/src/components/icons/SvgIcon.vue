66025d8c79608166c0aaac292d05b1bc29e85683
Konstantin <k.rom3066@gmail.com>
2025-03-26 19:14:47 +0600

    Create SvgHolder & SvgIcon

---
+++ portfolio-folder/src/components/icons/SvgIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  icon?: string | undefined
+  viewBox?: string,
+  height?: number,
+  width?: number,
+}>(), {
+  viewBox: '0 0 24 24',
+  height: 24,
+  width: 24,
+})
+</script>
+
+<template>
+  <svg :viewBox="viewBox" :height="height" :width="width">
+    <use :xlink:href="`#${icon}`" />
+  </svg>
+</template>
