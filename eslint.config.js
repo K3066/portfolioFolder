1dbcd123af1bfdacec9750f157b21532973e473e
Романчук Константин Дмитриевич <83108@npo.local>
2025-04-08 09:58:23 +0600

    Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,77 @@ export default [
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: {
+      "vue/max-attributes-per-line": ["warn", {
+        "singleline": {
+          "max": 1
+        },
+        "multiline": {
+          "max": 1
+        }
+      }],
+      "vue/html-self-closing": ["warn", {
+        "html": {
+          "void": "never",
+          "normal": "always",
+          "component": "always"
+        },
+        "svg": "always",
+        "math": "always"
+      }],
+      'vue/html-indent': [
+        'warn',
+        2,
+        {
+          attribute: 1,
+          baseIndent: 1,
+          closeBracket: 0,
+          alignAttributesVertically: true,
+          ignores: []
+        }
+      ],
+      "vue/component-name-in-template-casing": ["warn", "kebab-case", {
+        "registeredComponentsOnly": true,
+      }],
+      "vue/component-definition-name-casing": ["warn", "PascalCase"],
+      'vue/order-in-components': ['warn', {
+        order: [
+          'el',
+          'name',
+          'key',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          ['components', 'directives', 'filters'],
+          'extends',
+          'mixins',
+          ['provide', 'inject'],
+          'ROUTER_GUARDS',
+          'layout',
+          'middleware',
+          'validate',
+          'scrollToTop',
+          'transition',
+          'loading',
+          'inheritAttrs',
+          'model',
+          ['props', 'propsData'],
+          'emits',
+          'setup',
+          'asyncData',
+          'data',
+          'fetch',
+          'head',
+          'computed',
+          'watch',
+          'watchQuery',
+          'LIFECYCLE_HOOKS',
+          'methods',
+          ['template', 'render'],
+          'renderError'
+        ]
+      }],
+    }
   },
 
   {
@@ -16,7 +87,7 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
